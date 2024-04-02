772a52fcd2167442a9f3a07e961c787bae7e8150
bashir-site <59281683+bashir-site@users.noreply.github.com>
2024-04-03 00:34:14 +0300

    Update example2.py

--- example2.py
+++ example2.py
@@ -1,42 +1,50 @@
+from mytest import test_func
 
 
-def find_third_max_element(list1: list) -> int:
+def find_third_max_element1(list1: list) -> int:
     """
         Реализуй алгоритм, для поиска 3-его максимального
         элемента (бронза) + выпиши сложность.
     """
-    max1 = 0
-    i = 0
+    if len(list1)<=2:
+        return None
+    
+    i = 1
     while i <= len(list1) - 1:
-        if max1 < list1[i]:
-            max1 = list1[i]
+        if list1[0] < list1[i]:
+            list1[0], list1[i] = list1[i], list1[0]
+            
+        i += 1
+    
+    i = 2
+    while i <= len(list1) - 1:
+        if list1[1] < list1[i] and list1[1] != list1[i]:
+            list1[1], list1[i] = list1[i], list1[1]
         
         i += 1
     
-    max2 = 0
-    i = 0
+    i = 3
     while i <= len(list1) - 1:
-        if max2 < list1[i] < max1:
-            max2 = list1[i]
+        if list1[2] < list1[i] and list1[1] != list1[i]:
+            list1[2], list1[i] = list1[i], list1[2]
         
         i += 1
-    
-    
-    max3 = 0
-    i = 0
-    while i <= len(list1) - 1:
-        if max3 < list1[i] < max2 < max1:
-            max3 = list1[i]
-        
-        i += 1
-    
-    
-    
-    return max3
-        
+
+    return list1[2]
 
 
-list1 = [3, 1, 6, 5, 2, 4] # -> вывести 4
-list2 = [38, 42, 33, 71, 3, 21, 77, 99, 59, 63] # -> вывести 71
-list3 = [22, 100, 12, 41, 85, 79, 92, 79, 92, 89, 72, 34, 89, 44, 10] # -> вывести 89
-print(find_third_max_element(list1))
+def find_third_max_element2(list1: list) -> int:
+    """
+        Реализуй алгоритм, для поиска 3-его максимального
+        элемента (бронза) + выпиши сложность.
+    """
+    pass
+
+test_func(find_third_max_element1)
+
+
+list1 = [12, 84, -7, -13, 0, 321]
+print(find_third_max_element1(list1))
+
+
+
